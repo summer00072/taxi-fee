2619c67c43450edfe10f152964544fd457395a16
summer00072 <1573819207@qq.com>
2018-04-05 12:44:05 +0800

    taxiFee first

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function taxiFee(inputs) {
+    if (!inputs || inputs.distance <= 0 || inputs.parkingTime < 0) {
+    	return false;
+    }
+    let summary = 0;
+    let distance_value = inputs.distance;
+    if (distance_value <= 2) {
+    	summary = 6;
+    } else if (2 < distance_value && distance_value<= 8) {
+    	summary = 6 + ((distance_value - 2) * 0.8);
+    } else {
+    	summary = 10.8 + ((distance_value - 8) * 1.2);
+    }
+    let parkingTime_value = inputs.parkingTime;  
+    summary += (parkingTime_value * 0.25);
+    return Math.round(summary);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,30 @@
-const main = require('../main/main');
+const taxiFee = require('../main/main');
 
 describe('taxi fee', function () {
+
+	it('within 2km and parking' , function() {
+		let inputs = {distance:1,parkingTime:5};
+		let summary = taxiFee(inputs);
+		let expected = 7;
+		expect(summary).toEqual(expected);
+	});
+	it('between 2km and 8km and parking' , function() {
+		let inputs = {distance:7,parkingTime:10};
+		let summary = taxiFee(inputs);
+		let expected = 13;
+		expect(summary).toEqual(expected);
+	});
+	it('more than 8km and parking' , function() {
+		let inputs = {distance:10,parkingTime:20};
+		let summary = taxiFee(inputs);
+		let expected = 18;
+		expect(summary).toEqual(expected);
+	});
+	it('more than 8km and no parking' , function() {
+		let inputs = {distance:15,parkingTime:0};
+		let summary = taxiFee(inputs);
+		let expected = 19;
+		expect(summary).toEqual(expected);
+	});
     // write your tests here...
 });
